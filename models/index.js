863c13bf313bb5192fb72c571a74ccb0b4325385
Alexandra Buceceanu <abuceceanu@dvloper.io>
2025-02-22 14:06:22 +0000

    Updated .gitignore and cleared cache

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const User = require('./User');
 const Department = require('./Department');
 const DepartmentManager = require('./DepartmentManager');
 
-// Ensure all models are initialized
 const initModels = async () => {
   await sequelize.sync({ force: true });
   console.log(' All models synchronized successfully.');
