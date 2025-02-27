82f6395dfba7cc27716c19e772b6474a3564d67c
Alexandra Buceceanu <abuceceanu@dvloper.io>
2025-02-27 10:56:18 +0000

    integrated keycloak with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import keycloak from './keycloak';
+import Home from './components/Home';
+import AdminPage from './components/AdminPage';
+import HelloWorld from './components/HelloWorld';
+import ItemList from './components/ItemList';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const [keycloakInitialized, setKeycloakInitialized] = useState(false);
+
+  useEffect(() => {
+    keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+      setKeycloakInitialized(true);
+    });
+  }, []);
+
+  return (
+    <ReactKeycloakProvider authClient={keycloak}>
+      <ToastContainer />
+      {keycloakInitialized ? (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route
+              path="/hello-world"
+              element={
+                <div className="App">
+                  <HelloWorld />
+                  <ItemList />
+                </div>
+              }
+            />
+          </Routes>
+        </Router>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </ReactKeycloakProvider>
+  );
+};
+
+export default App;
+/*
 import React from 'react';
 import HelloWorld from './components/HelloWorld';
 import ItemList from './components/ItemList';
@@ -16,4 +64,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App; */
---
+++ frontend/src/keycloak.js
@@ -0,0 +1,21 @@
+import Keycloak from 'keycloak-js';
+
+const keycloak = new Keycloak({
+    url: process.env.REACT_APP_KEYCLOAK_URL,
+    realm: process.env.REACT_APP_KEYCLOAK_REALM,
+    clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID
+});
+
+keycloak.onTokenExpired = () => {
+    keycloak.updateToken(30).then((refreshed) => {
+        if (refreshed) {
+            console.log(' Token refreshed');
+        } else {
+            console.warn('Token not refreshed, user is still logged in');
+        }
+    }).catch(() => {
+        console.error(' Failed to refresh token');
+    });
+};
+
+export default keycloak;
